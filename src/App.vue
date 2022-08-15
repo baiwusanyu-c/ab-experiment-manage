c8bc553dcb01eceadf2c07c6795026c2dff4f14d
陈智辉 <18281682921@163.com>
2022-08-15 17:30:02 +0800

    chore: 若依模板vue3版本初始化提交

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import useSettingsStore from './store/modules/settings'
+import { handleThemeStyle } from './utils/theme'
+import {defineComponent,onMounted,nextTick} from "vue";
+export default defineComponent({
+  setup(){
+    onMounted(() => {
+      nextTick(() => {
+        // 初始化主题样式
+        handleThemeStyle(useSettingsStore().theme)
+      })
+    })
+    return {
+
+    }
+  }
+})
+
+</script>
